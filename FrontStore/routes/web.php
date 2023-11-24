ab74ac4584dc9e61910ebae723ab16b542cd832c
prado <pradojimenezmaria@gmail.com>
2023-11-24 14:34:08 -0600

    BD terminada

--- FrontStore/routes/web.php
+++ FrontStore/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductosController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/',[ProductosController::class,'index'])->name('index');
